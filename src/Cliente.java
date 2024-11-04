e2f654803a33bc63ebae5d154e1b150f277088a7
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-04 14:05:03 -0300

    Implementação de todas as classes e adição dos métodos

---
+++ src/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+
+    private int agencia;
+    private int numero;
+    private String nome;
+
+    
+
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,19 @@
+public class ContaPoupanca {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+        
+    }
+
+    public void transferir(){
+        
+    }
+
+}
